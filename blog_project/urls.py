861010cf808658b6cee254c52cd58cdce98ee1be
Prashant Sethi <theseth.prashant@gmail.com>
2020-05-30 23:53:54 +0530

    Added user profile and profile pic functionality

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,7 @@ urlpatterns = [
          name='user-logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
